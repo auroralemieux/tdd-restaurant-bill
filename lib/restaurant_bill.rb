4a114c510f00e6ccf64211ff9cc1ba9755cac82f
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-27 11:03:20 -0800

    summing and adding tax

--- lib/restaurant_bill.rb
+++ lib/restaurant_bill.rb
@@ -1,12 +1,22 @@
-# class RestaurantBill
-#   attr_reader :ordered_items
+class RestaurantBill
+  attr_reader :ordered_items
 
-#   def initialize
-#     @ordered_items = []
-#   end
+  def initialize
+    @ordered_items = []
+  end
 
-#   def order_item(item, cost)
-#     @ordered_items << [item, cost]
-#   end
+  def order_item(item, cost)
+    @ordered_items << [item, cost]
+  end
 
-# end
+  def sum_and_add_tax(tax_rate)
+    sum = 0
+    @ordered_items.each do |item|
+      sum += item[1].to_f
+    end
+    total = sum + (sum * tax_rate).to_f
+    total.round(2)
+  end
+
+
+end
--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,40 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+
+describe RestaurantBill do
+
+  before do
+    @my_bill = RestaurantBill.new
+  end
+
+  it "A bill is created with an empty ordered_items" do
+    @my_bill.ordered_items.must_be_instance_of Array
+    @my_bill.ordered_items.empty?.must_equal true
+  end
+
+  it "after running order_item the ordered_items array should include the new item" do
+    @my_bill.ordered_items.length.must_equal 0
+    @my_bill.order_item("pancake", 12.99)
+    @my_bill.ordered_items.length.must_equal 1
+    @my_bill.ordered_items[0].class.must_equal Array
+  end
+
+  it "after ordering 3 things the array should have a length of 3" do
+    @my_bill.ordered_items.length.must_equal 0
+    @my_bill.order_item("pancake", 12.99)
+    @my_bill.order_item("gravyballs", 77.01)
+    @my_bill.order_item("ice cream", 1.00)
+    @my_bill.ordered_items.length.must_equal 3
+    @my_bill.ordered_items[0].class.must_equal Array
+  end
+
+  it "totals plus tax" do
+    @my_bill.order_item("pancake", 3)
+    @my_bill.order_item("pancake", 4)
+    @my_bill.order_item("pancake", 7)
+    @my_bill.sum_and_add_tax(0.096).must_equal 15.34
+  end
+
+end
